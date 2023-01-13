0c12a687695f4840a355ea6752ef90b7f2b8bca4
Mihai0976 <mihai.sorinescu@ha.ax>
2023-01-13 20:55:30 +0200

    Add functinality that changes the color of butons

--- counter/app.js
+++ counter/app.js
@@ -1,30 +1,38 @@
 //set initial count
 let count = 0;
 
- // select value and buttons
+// select value and buttons
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
- 
+
 console.log(btns);
 
-btns.forEach(function (btn) { 
+btns.forEach(function (btn) {
     btn.addEventListener("click", function (e) {
         const styles = e.currentTarget.classList;
         if (styles.contains('decrease')) {
             count--;
         } else if (styles.contains('increase')) {
+            btn.style.color = "green";
             count++;
         } else {
             count = 0;
         }
 
+        //change color of counter and btn depending on counter's value
         if (count > 0) {
             value.style.color = "green";
-            btn.style.color = "green";
-        }else if (count < 0) {
+            if (count < 0) {
+                btn.style.color = "red";
+            }
+
+        } else if (count < 0) {
             value.style.color = "red";
             btn.style.color = "red";
-        }else if (count === 0) {
+            if (count > 0) {
+                btn.style.color = "green";
+            }
+        } else if (count === 0) {
             value.style.color = "blue";
             btn.style.color = "blue";
         }
