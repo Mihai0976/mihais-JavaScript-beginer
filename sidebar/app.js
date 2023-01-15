e5f6e1939b97db420061b759143003b855b51f1a
Mihai0976 <mihai.sorinescu@ha.ax>
2023-01-16 01:21:29 +0200

    Add functionality to show or hide sidebar

--- sidebar/app.js
+++ sidebar/app.js
@@ -1,1 +1,17 @@
-create JavaScript file 
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+
+toggleBtn.addEventListener("click", function () {
+   /* if (sidebar.classList.contains("show-sidebar")) {
+        sidebar.classList.remove("show-sidebar");
+    } else {
+        sidebar.classList.add("show-sidebar");
+    }*/
+
+    sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+        sidebar.classList.remove("show-sidebar");   
+})
