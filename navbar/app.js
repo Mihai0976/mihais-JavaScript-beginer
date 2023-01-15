c321c9d2bdee690054d95be46904b3f5ab9a1041
Mihai0976 <mihai.sorinescu@ha.ax>
2023-01-15 18:32:45 +0200

    Created functionality to show or remove class

--- navbar/app.js
+++ navbar/app.js
@@ -3,3 +3,22 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navTogle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navTogle.addEventListener("click", function () {
+    //console.log(links.classList);
+    //console.log(links.classList.contains('random')); 
+    //console.log(links.classList.contains("links")); 
+    // add and remove class with add remove methods
+    /* if (links.classList.contains("show-links")) {
+         links.classList.remove("show-links");
+     } else {
+         links.classList.add("show-links");
+      }*/
+    
+    // add and remove class with toggle method
+    links.classList.toggle("show-links");
+    
+});
