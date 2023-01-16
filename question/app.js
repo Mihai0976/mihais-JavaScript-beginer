d17922c6e59202580986d742eb261871fde91149
Mihai0976 <mihai.sorinescu@ha.ax>
2023-01-16 21:18:10 +0200

    Add funct to buttons usig the selector inside elem

--- question/app.js
+++ question/app.js
@@ -1,4 +1,5 @@
 // traversing the dom
+/*
 const btns = document.querySelectorAll(".question-btn");
 
 btns.forEach(function (btn) {
@@ -6,6 +7,22 @@ btns.forEach(function (btn) {
         const question = e.currentTarget.parentElement.parentElement;
         question.classList.toggle("show-text");
     });
-});
- 
+});*/
+
 //using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+    //console.log(question)
+    const btn = question.querySelector(".question-btn");
+
+    btn.addEventListener("click", function () {
+       questions.forEach(function(item) {
+          if (item !== question) {
+              item.classList.remove("show-text");
+          }
+       })
+
+        question.classList.toggle("show-text");
+    })
+});
