cdd1bebe56fda71957aea5d0a1b57ed0853c9c2c
Mihai0976 <mihai.sorinescu@ha.ax>
2023-01-23 19:49:30 +0200

    Add function to display buttons dinamicaly

--- menu/app.js
+++ menu/app.js
@@ -2,7 +2,7 @@ const menu = [
   {
     id: 1,
     title: "Introduction to AI",
-    category: "ai",
+    category: "AI",
     price: 1000,
     img: "./images/ai.jpg",
     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste? `,
@@ -10,7 +10,7 @@ const menu = [
   {
     id: 2,
     title: "Software Engineering",
-    category: "softwaredevelopment",
+    category: "software-development",
     price: 2000,
     img: "./images/softeng1.jpg",
     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste?`,
@@ -26,7 +26,7 @@ const menu = [
   {
     id: 3,
     title: "System requirements and Product Management",
-    category: "Product Mnagement & Development",
+    category: "Management",
     price: 2000,
     img: "./images/projmanagement.jpeg",
     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste?`,
@@ -34,16 +34,16 @@ const menu = [
   {
     id: 5,
     title: "Cloud Development With AWS",
-    category: "softwaredevelopment",
-    price: 22.99,
+    category: "software-development",
+    price: 800,
     img: "./images/cloud.png",
     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste?`,
   },
   {
     id: 6,
     title: " Agile Methodology in Project Management",
-    category: "Product Mnagement & Development",
-    price: 3.500,
+    category: "Management",
+    price: 1500,
     img: "./images/agile.jpg",
     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste?`,
   },
@@ -51,14 +51,14 @@ const menu = [
     id: 7,
     title: "Machine Learning With Python",
     category: "AI",
-    price: 8.99,
+    price: 1900,
     img: "./images/machine.png",
     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste? `,
   },
   {
     id: 8,
     title: "Data Base Design",
-    category: "softwaredevelopment",
+    category: "software-development",
     price: 700,
     img: "./images/databasedesign.jpeg",
     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste? `,
@@ -66,44 +66,35 @@ const menu = [
   {
     id: 9,
     title: "Digital Product Management",
-    category: "Product Mnagement & Development",
-    price: 16.99,
+    category: "Management",
+    price: 2100,
     img: "./images/digitalproc.jpg",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste?`,
+  },
+  {
+    id: 10,
+    title: "Beginner To Professional UX Designer",
+    category: "ui/ux",
+    price: 2600,
+    img: "./images/uxdesigner.jpg",
     desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste?`,
   },
 ];
 
 
 const sectionCenter = document.querySelector(".section-center");
-const filterBtns = document.querySelectorAll(".filter-btn")
+const container = document.querySelector(".btn-container");
+
 
 
 //load items
 window.addEventListener('DOMContentLoaded', function () {
   displatMenuItems(menu);
+  displayMenuButtons();
+  selectButtons()
+  //console.log(categoryBtns);
+
 });
-
-
-//filter items
-filterBtns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const category = e.currentTarget.dataset.id;
-    const menuCategory = menu.filter(function (menuItem) {
-      if (menuItem.category === category) {
-        return menuItem;
-      }
-
-    });
-    if (category === 'All') {
-      displatMenuItems(menu);
-    } else {
-      displatMenuItems(menuCategory);
-    }
-    //console.log(menuCategory.category);
-  })
-});
-
-
 
 
 function displatMenuItems(menuItems) {
@@ -124,4 +115,19 @@ function displatMenuItems(menuItems) {
   });
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
-}
+};
+
+//display buttons
+function displayMenuButtons() {
+  const categories = menu.reduce(function (values, item) {
+    if (!values.includes(item.category)) {
+      values.push(item.category);
+    }
+    return values;
+  }, ["All"]);
+  //console.log(categories);
+  const categoryBtns = categories.map(function (category) {
+    return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`;
+  }).join("");
+  container.innerHTML = categoryBtns;
+};
