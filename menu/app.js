80727d9e00fc24d770f1a9fc32e761ba26cbfc61
Mihai0976 <mihai.sorinescu@ha.ax>
2023-01-23 13:27:10 +0200

    Add functionality in app.js to show all items

--- menu/app.js
+++ menu/app.js
@@ -1,74 +1,122 @@
 const menu = [
   {
     id: 1,
-    title: "buttermilk pancakes",
-    category: "breakfast",
-    price: 15.99,
-    img: "./images/item-1.jpeg",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    title: "Introduction to AI",
+    category: "ai",
+    price: 1000,
+    img: "./images/ai.jpg",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste? `,
   },
   {
     id: 2,
-    title: "diner double",
-    category: "lunch",
-    price: 13.99,
-    img: "./images/item-2.jpeg",
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
-  },
-  {
-    id: 3,
-    title: "godzilla milkshake",
-    category: "shakes",
-    price: 6.99,
-    img: "./images/item-3.jpeg",
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    title: "Software Engineering",
+    category: "softwaredevelopment",
+    price: 2000,
+    img: "./images/softeng1.jpg",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste?`,
   },
   {
     id: 4,
-    title: "country delight",
-    category: "breakfast",
-    price: 20.99,
-    img: "./images/item-4.jpeg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    title: "Advanced AI Dev With Python",
+    category: "AI",
+    price: 2500,
+    img: "./images/aipython.jpg",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste?`,
+  },
+  {
+    id: 3,
+    title: "System requirements and Product Management",
+    category: "Product Mnagement & Development",
+    price: 2000,
+    img: "./images/projmanagement.jpeg",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste?`,
   },
   {
     id: 5,
-    title: "egg attack",
-    category: "lunch",
+    title: "Cloud Development With AWS",
+    category: "softwaredevelopment",
     price: 22.99,
-    img: "./images/item-5.jpeg",
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    img: "./images/cloud.png",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste?`,
   },
   {
     id: 6,
-    title: "oreo dream",
-    category: "shakes",
-    price: 18.99,
-    img: "./images/item-6.jpeg",
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    title: " Agile Methodology in Project Management",
+    category: "Product Mnagement & Development",
+    price: 3.500,
+    img: "./images/agile.jpg",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste?`,
   },
   {
     id: 7,
-    title: "bacon overflow",
-    category: "breakfast",
+    title: "Machine Learning With Python",
+    category: "AI",
     price: 8.99,
-    img: "./images/item-7.jpeg",
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    img: "./images/machine.png",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste? `,
   },
   {
     id: 8,
-    title: "american classic",
-    category: "lunch",
-    price: 12.99,
-    img: "./images/item-8.jpeg",
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    title: "Data Base Design",
+    category: "softwaredevelopment",
+    price: 700,
+    img: "./images/databasedesign.jpeg",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste? `,
   },
   {
     id: 9,
-    title: "quarantine buddy",
-    category: "shakes",
+    title: "Digital Product Management",
+    category: "Product Mnagement & Development",
     price: 16.99,
-    img: "./images/item-9.jpeg",
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    img: "./images/digitalproc.jpg",
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus esse impedit, iure necessitatibus neque iste?`,
   },
 ];
+
+/*const sectionCenter = document.querySelector(".section-center");
+
+window.addEventListener("DOMContentLoaded", function () {
+  let displayMenu = menu.map(function (item) {
+    // console.log(item);
+
+    return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  console.log(displayMenu);
+
+  sectionCenter.innerHTML = displayMenu;
+});*/
+const sectionCenter = document.querySelector(".section-center");
+
+window.addEventListener('DOMContentLoaded', function () {
+  //console.log("shake and bake");
+  let displayMenu = menu.map(function (item) {
+    return ` 
+    <article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`
+    ;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+});
